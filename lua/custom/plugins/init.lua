bbc2b0dd82c8d32630a31fc5ce8e3004830c26b1
Atul Patil <atulpatildbz@gmail.com>
2025-01-18 12:31:37 +0530

    move oil to custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,42 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  -- File explorer that lets you edit your filesystem like a buffer
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+      default_file_explorer = true,
+      -- Skip confirmation for actions like deletion
+      skip_confirm_for_simple_edits = true,
+      -- Show hidden files
+      view_options = {
+        show_hidden = true,
+      },
+      -- Keymaps in oil buffer
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-v>'] = 'actions.select_vsplit',
+        ['<C-s>'] = 'actions.select_split',
+        ['<C-t>'] = 'actions.select_tab',
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['<C-r>'] = 'actions.refresh',
+        ['-'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = 'actions.tcd',
+        ['gs'] = 'actions.change_sort',
+        ['gx'] = 'actions.open_external',
+        ['g.'] = 'actions.toggle_hidden',
+      },
+      -- Set to false to disable all of the above keymaps
+      use_default_keymaps = false,
+    },
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+}
